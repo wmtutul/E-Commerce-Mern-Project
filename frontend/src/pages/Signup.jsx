983ee325e9634d02a955d42c8ab425a8bc8a351b
wmtutul <wmtutul@gmail.com>
2026-03-03 23:35:38 +0600

    Signup page done frontend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,30 +1,78 @@
-import React, { useState } from 'react'
+import { useState } from "react";
+import api from "../api/axios";
 
-const Signup = () => {
+export default function Signup() {
+  const [form,setForm]=useState({
+    name:"",
+    email:"",
+    password:""
+  })
+  const [msg,setMsg]=useState("");
 
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    password: ""
-  });
-
-  const handleChange = (e) => {
-
+  const handleChange=(e)=>{
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value
+    });
   }
 
-  const handleSubmit = async (e) => {
+  const handleSubmit=async(e)=>{
     e.preventDefault();
+
+    try{
+      const response=await api.post("/auth/signup",form);
+      setMsg(response.data.message);
+    } catch(err){
+      setMsg(err.response?.data?.message || "An error occurred" );
+    }
   }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+      <div  className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
         <h2 className="text-2xl font-bold mb-6 text-center">Create Account</h2>
 
+        {msg && (
+          <div className="mb-4 text-center text-sm text-blue-600 font-medium">
+            {msg}
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            name='name'
+            placeholder="Enter Name"
+            value={form.name}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+          <input
+            name='email'
+            type="email"
+            placeholder="Enter Email"
+            value={form.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+          <input
+            name='password'
+            type="password"
+            placeholder="Enter Password"
+            value={form.password}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors cursor-pointer"
+          >
+            Sign Up
+          </button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
-
-export default Signup
-
